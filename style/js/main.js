aa092f920e1733b1e4b0c7f4ebddf099518378b4
Joyqul Chen <joy1357924680@gmail.com>
2015-12-14 08:55:23 +0800

    fixed column propotion in web view

--- style/js/main.js
+++ style/js/main.js
@@ -1,4 +1,13 @@
 $(document).ready(function(){
+	$(window).resize(function(){
+		var w = $(window).innerWidth();
+		if (w > 768) {
+            $("nav").show();
+		}
+        else {
+            $("nav").hide();
+        }
+	});
     $(".menu").click(function() {
 		$("nav").toggle();
 	});
